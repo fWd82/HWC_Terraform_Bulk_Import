542e845b8eb99666201556729182f69e5cfc3406
Fawad Iqbal <fwd82@live.com>
2026-03-10 14:04:47 +0400

    I need the file changes (diffs) to generate a commit message.

---
+++ Other Files/Import_Blocks.tf
@@ -0,0 +1,516 @@
+# --- Provider: ASTROZERO ---
+import {
+  to = huaweicloud_astrozero_instances.astrozero
+  id = "4bceed196ba0eed72b3a72434f369679"
+}
+
+# --- Provider: CBR ---
+import {
+  to = huaweicloud_cbr_policy.defaultpolicy
+  id = "45da0a44-6cd6-44c8-bb3c-d2d8f4161f91"
+}
+
+import {
+  to = huaweicloud_cbr_policy.defaultpolicy_1
+  id = "4d4ab1a6-9218-48e1-8f1f-3ab1a6326761"
+}
+
+import {
+  to = huaweicloud_cbr_policy.defaultpolicy_2
+  id = "5253ae11-2730-4444-b742-f594e5f13271"
+}
+
+import {
+  to = huaweicloud_cbr_policy.defaultpolicy_3
+  id = "6299879a-31e8-4d19-981f-698b26c6b87a"
+}
+
+# --- Provider: CDN ---
+import {
+  to = huaweicloud_cdn_domains.myobsfawadiqbalme
+  id = "2185d10dc75445afb14e119acfa5b4da"
+}
+
+import {
+  to = huaweicloud_cdn_domains.expressnewsfawadiqbalcom
+  id = "3abf772ce8dc4496ac959806df095de2"
+}
+
+import {
+  to = huaweicloud_cdn_domains.fawadiqbalcom
+  id = "66cc53deaf5f434483ec7aa508822541"
+}
+
+# --- Provider: CODEARTSBUILD ---
+import {
+  to = huaweicloud_codeartsbuild_cloud_build_server.python_ddemo_test_23063533
+  id = "3e58a5d4c2b2418198c0de2f41eaee91"
+}
+
+import {
+  to = huaweicloud_codeartsbuild_cloud_build_server.python_ddemo_test_cloudbuild
+  id = "69ffa3baf65f4b9ab65c00de87e75e1d"
+}
+
+# --- Provider: CODEARTSPIPELINE ---
+import {
+  to = huaweicloud_codeartspipeline_pipelines.python_ddemo_test_pipeline
+  id = "7d30a8c8b7374caf9de936f3b319ef9e"
+}
+
+# --- Provider: CTS ---
+import {
+  to = huaweicloud_cts_trackers.system
+  id = "19a77552-19af-4ebb-95aa-c67defc297f4"
+}
+
+import {
+  to = huaweicloud_cts_trackers.system_1
+  id = "7bebdc7c-f2c1-408a-a461-8b01463b3623"
+}
+
+# --- Provider: ER ---
+import {
+  to = huaweicloud_er_instances.er_97e0
+  id = "6d753dee-f4ab-4d12-b199-cc13d3563a74"
+}
+
+# --- Provider: IAM ---
+import {
+  to = huaweicloud_iam_agencies.vod_admin_trust_4f0386f737fb4ad38e80006222f86b02
+  id = "089fe633ec464f28ba87d0f07127b597"
+}
+
+import {
+  to = huaweicloud_iam_agencies.medialive_admin_trust
+  id = "0b623a59d0214b899392919047e3ee64"
+}
+
+import {
+  to = huaweicloud_iam_agencies.pipeline_admin_trust
+  id = "0cfe5d51cdc94de587b332af8919c390"
+}
+
+import {
+  to = huaweicloud_iam_agencies.asm_admin_trust
+  id = "1055d0eef7f64d84910ca59fb622f5ba"
+}
+
+import {
+  to = huaweicloud_iam_agencies.evsaccesskms
+  id = "13dfce2783a24bdb90b981e40409677d"
+}
+
+import {
+  to = huaweicloud_iam_roles.policy_razu
+  id = "151095c5283047f98ae51ab90c65bfe7"
+}
+
+import {
+  to = huaweicloud_iam_agencies.servicelinkedagencyforidentitycenter
+  id = "1671be59d6354fee94ce715d1625a9f7"
+}
+
+import {
+  to = huaweicloud_iam_roles.cce_cluster_policies
+  id = "17d9f388f34e40d78da6fc2088bbd795"
+}
+
+import {
+  to = huaweicloud_iam_users.adrian
+  id = "1b0bfa5fc90946a78c1216ffa2200326"
+}
+
+import {
+  to = huaweicloud_iam_agencies.cce_cluster_agency
+  id = "1caef52cdaca4e2c8330215aa8813e4a"
+}
+
+import {
+  to = huaweicloud_iam_agencies.servicelinkedagencyforsecmaster
+  id = "1fbc16404f2e45adbc2be2f61692ffbd"
+}
+
+import {
+  to = huaweicloud_iam_groups.om_group
+  id = "20c05ede12814a5e8435edb1262c86d7"
+}
+
+import {
+  to = huaweicloud_iam_roles.pangu_obswriteonly
+  id = "20ef9df40d5c4495a6b6625f74276fac"
+}
+
+import {
+  to = huaweicloud_iam_users.sftpgo_user
+  id = "212eb827af0a40b196bc06986bd852ff"
+}
+
+import {
+  to = huaweicloud_iam_agencies.dli_admin_agency
+  id = "283030761c2f4c30aa973a83dbda7111"
+}
+
+import {
+  to = huaweicloud_iam_roles.cce_admin_policies
+  id = "286cf4dfed174ced8691545e3d85a699"
+}
+
+import {
+  to = huaweicloud_iam_agencies.cts_admin_trust
+  id = "29816fcd144141d2810848c585e63196"
+}
+
+import {
+  to = huaweicloud_iam_users.fawad
+  id = "2aa218ecac2944ef83521fb9a0018e3a"
+}
+
+import {
+  to = huaweicloud_iam_groups.custom_bucket_test
+  id = "2afcc22655524d019f4da3c6045cf2d8"
+}
+
+import {
+  to = huaweicloud_iam_users.pankaj
+  id = "2e0e3e90d97247c7b03e67108335f5ab"
+}
+
+import {
+  to = huaweicloud_iam_agencies.agency_fg_all
+  id = "2e3c0f89c83d4c79b518e403f4633bfc"
+}
+
+import {
+  to = huaweicloud_iam_users.zdravko
+  id = "344435c52bac4274bfec61075d55f7f0"
+}
+
+import {
+  to = huaweicloud_iam_roles.elb_to_waf_operate_policy
+  id = "35e87f4bc9d44f669245fed526c3c077"
+}
+
+import {
+  to = huaweicloud_iam_roles.asm_admin_trust_policy
+  id = "35f68956b08c45908fb6972bacad1ebf"
+}
+
+import {
+  to = huaweicloud_iam_roles.fgs_default_region_role
+  id = "38d67b9b1a76435cb0bc2ce9dce052e0"
+}
+
+import {
+  to = huaweicloud_iam_agencies.modelarts_agency
+  id = "3cfc1d22190f45ecb703bfa5e6a761be"
+}
+
+import {
+  to = huaweicloud_iam_roles.policy_restrict_ecs
+  id = "3fd14e043bf843579d757ad52c72ba48"
+}
+
+import {
+  to = huaweicloud_iam_groups.archformation_group
+  id = "401ccb07a22542398e29dc5deeb4349a"
+}
+
+import {
+  to = huaweicloud_iam_users.newuser
+  id = "40da35eb5b57455db4018c9eaec78719"
+}
+
+import {
+  to = huaweicloud_iam_agencies.ssa_admin_trust
+  id = "439e030836134497a3e47e8d196f462b"
+}
+
+import {
+  to = huaweicloud_iam_agencies.secmaster_agency
+  id = "45e4979d8dcb49f08f1b425ce07add59"
+}
+
+import {
+  to = huaweicloud_iam_agencies.workspace_admin_trust
+  id = "489dfae4b604405989ef59f0da9982b4"
+}
+
+import {
+  to = huaweicloud_iam_roles.queryanddownloadscm
+  id = "48c16d3d17704f33bcf9846111c0f78b"
+}
+
+import {
+  to = huaweicloud_iam_groups.db_group
+  id = "4d958d0a49b44c53ae603b8d48eb738b"
+}
+
+import {
+  to = huaweicloud_iam_users.aimalexe
+  id = "4f437b1b124545b6a02ae1f5d2d8b8d4"
+}
+
+import {
+  to = huaweicloud_iam_users.user0
+  id = "5017634bf12a47dabed986c5ff034f30"
+}
+
+import {
+  to = huaweicloud_iam_policies.obs_obs_bucket
+  id = "5403ef2dccde4b6fb7e4d13909ca968f"
+}
+
+import {
+  to = huaweicloud_iam_groups.limitedaccess_temp
+  id = "57e68441c535424c8970109e932fab4b"
+}
+
+import {
+  to = huaweicloud_iam_groups.airlivedrive1_ug
+  id = "588c9bf4931e4e35afc0ebc414fa78b9"
+}
+
+import {
+  to = huaweicloud_iam_agencies.servicestage_trust
+  id = "63872747cf184792896bfe50f1451a51"
+}
+
+import {
+  to = huaweicloud_iam_agencies.obs_agency
+  id = "656e540ae39843c597072603128b13c4"
+}
+
+import {
+  to = huaweicloud_iam_agencies.ei_pangulargemodels_trust
+  id = "6ba2f524dbf4469881a255e03969fa14"
+}
+
+import {
+  to = huaweicloud_iam_groups.cicd_github_actions
+  id = "6bb31a615262487e9f4703771ac07958"
+}
+
+import {
+  to = huaweicloud_iam_agencies.hilens_admin_trust
+  id = "6ea8519792bf4b5fae1083919d3b98f6"
+}
+
+import {
+  to = huaweicloud_iam_agencies.aos_admin_trust
+  id = "6fb5a1a45e814eb4b84e81fdeb3608ed"
+}
+
+import {
+  to = huaweicloud_iam_roles.fgs_default_global_role
+  id = "75f6f28e9ca942a8b03e7d6c06cdeaa7"
+}
+
+import {
+  to = huaweicloud_iam_roles.modelarts_policy_8gi19x
+  id = "794296b6c00542eab578fd2a42091a21"
+}
+
+import {
+  to = huaweicloud_iam_roles.policy_block_obs
+  id = "7c151122c82f4e29bef303a1cad84714"
+}
+
+# --- Provider: IMS ---
+import {
+  to = huaweicloud_ims_images.windows_server_standard_2022
+  id = "02c2f51f-4b0c-4d4b-b61d-84cf04c16394"
+}
+
+import {
+  to = huaweicloud_ims_images.ims_ecs_69ef_8511_v1_20250707_111316
+  id = "04fee844-cdc1-4fe0-b1b8-babb955fe22d"
+}
+
+import {
+  to = huaweicloud_ims_images.image_win_ser_2016_vm
+  id = "1a9a6133-8268-40ff-b61c-9ad45da2e053"
+}
+
+import {
+  to = huaweicloud_ims_images.windows_server_standard_2019
+  id = "2c212efd-ae3d-4721-9e82-72db632b9cc7"
+}
+
+import {
+  to = huaweicloud_ims_images.ims_ecs_69ef_8511_v2_20250707_0914
+  id = "6d19a714-21be-46fd-abcc-83782bc108e0"
+}
+
+# --- Provider: KMS ---
+import {
+  to = huaweicloud_kms_keys.kpsdefault
+  id = "0c18fea2-d156-4ff4-996d-c6516b19d531"
+}
+
+import {
+  to = huaweicloud_kms_keys.csmsdefault
+  id = "75ed1272-9902-4f3a-8d6b-8be65d3b113a"
+}
+
+# --- Provider: LTS ---
+import {
+  to = huaweicloud_lts_topics.lts_topic_topic1
+  id = "47a57b66-f190-4772-ad90-652fd8229b4f"
+}
+
+# --- Provider: ORGANIZATIONS ---
+import {
+  to = huaweicloud_organizations_account.his_fawad
+  id = "4f0386f737fb4ad38e80006222f86b02"
+}
+
+# --- Provider: VPC ---
+import {
+  to = huaweicloud_vpc_networks.subnet_2
+  id = "00efaab7-1b1e-4951-af3e-6e5a14b280b6"
+}
+
+import {
+  to = huaweicloud_vpc_vpcs.vpc_default
+  id = "0118768a-9c89-49e5-b503-ac3c179856ee"
+}
+
+import {
+  to = huaweicloud_vpc_security_groups.default
+  id = "022679f7-acad-4ddc-8dad-69a4af36f192"
+}
+
+import {
+  to = huaweicloud_vpc_vpcs.vpc_default_1
+  id = "0288e8b0-e494-4053-94a7-cea1951c8986"
+}
+
+import {
+  to = huaweicloud_vpc_security_groups.default_1
+  id = "097ec158-1d97-4872-aede-33f352c4c48d"
+}
+
+import {
+  to = huaweicloud_vpc_vpcs.vpc_default_smb
+  id = "0bf0917c-ec50-4833-991b-5c9358a9e534"
+}
+
+import {
+  to = huaweicloud_vpc_vpcs.vpc_default_smb_1
+  id = "0c799297-0d92-4aa7-bbb8-69a5a26425de"
+}
+
+import {
+  to = huaweicloud_vpc_security_groups.default_2
+  id = "0ce6e8f1-e74c-492d-b630-c4a94d123b76"
+}
+
+import {
+  to = huaweicloud_vpc_route_tables.rtb_vpc_default
+  id = "0dcb0ff1-c427-44f3-848e-d1a5ee4fb89c"
+}
+
+import {
+  to = huaweicloud_vpc_networks.subnet_default
+  id = "10447e0f-0ba8-4435-9caf-b311a93ed000"
+}
+
+import {
+  to = huaweicloud_vpc_security_groups.sg_default_smb
+  id = "1d8da586-7106-4417-a357-12f086e7a291"
+}
+
+import {
+  to = huaweicloud_vpc_route_tables.rtb_vpc_default_1
+  id = "25ec0567-5cf8-4eba-b309-20c4509f9331"
+}
+
+import {
+  to = huaweicloud_vpc_security_groups.default_3
+  id = "28dc59a8-16bf-47a2-9d5d-ecf7cdeece73"
+}
+
+import {
+  to = huaweicloud_vpc_security_groups.default_4
+  id = "310c5313-4f11-4193-b4c2-e8544ec57480"
+}
+
+import {
+  to = huaweicloud_vpc_networks.subnet_default_1
+  id = "3c0fa32d-bea0-4555-8f04-23d8fb25b3f3"
+}
+
+import {
+  to = huaweicloud_vpc_vpcs.vpc_default_2
+  id = "3cf58bfe-4135-4716-ae39-47e005327c86"
+}
+
+import {
+  to = huaweicloud_vpc_vpcs.vpc_default_3
+  id = "4195c71f-7ff5-4953-89d8-9ecd354fa716"
+}
+
+import {
+  to = huaweicloud_vpc_security_groups.default_5
+  id = "42b2e7b0-0217-4148-9f9f-ce49b51bc158"
+}
+
+import {
+  to = huaweicloud_vpc_security_groups.sg_c074
+  id = "4aac4f26-2bc9-478d-97ce-7f52a3cd1e85"
+}
+
+import {
+  to = huaweicloud_vpc_route_tables.rtb_vpc_default_2
+  id = "53875336-fd1c-4c9d-97cf-538f4c73f869"
+}
+
+import {
+  to = huaweicloud_vpc_networks.subnet_default_2
+  id = "55700684-d37d-427e-8dcb-99aa0fc5b5d7"
+}
+
+import {
+  to = huaweicloud_vpc_route_tables.rtb_vpc_default_smb
+  id = "57ab43a8-fd57-49f7-8246-650804843f56"
+}
+
+import {
+  to = huaweicloud_vpc_networks.subnet_default_3
+  id = "57de869a-e605-40e7-801a-306ed3725d5a"
+}
+
+import {
+  to = huaweicloud_vpc_security_groups.default_6d76
+  id = "6d76f247-2d8d-4b25-ad62-4d3f5a651bd5"
+}
+
+import {
+  to = huaweicloud_vpc_security_groups.default_6fb5
+  id = "6fb51e4b-e47a-4a82-821d-53b006389622"
+}
+
+import {
+  to = huaweicloud_vpc_security_groups.sg_default_smb_1
+  id = "70e22bb6-c6c0-4045-88ab-a276c345a3dc"
+}
+
+import {
+  to = huaweicloud_vpc_security_groups.default_7230
+  id = "72303dcc-bdcf-4980-832f-2d29b973f798"
+}
+
+import {
+  to = huaweicloud_vpc_vpcs.vpc_default_4
+  id = "78add4f6-8ea7-436d-9de2-30dae0320f98"
+}
+
+# --- Provider: VPCEP ---
+import {
+  to = huaweicloud_vpcep_endpoint_services.me_east_1cloudide3f2d9394_34c8_49a7_a72a_6c2cd1da83ca
+  id = "3f2d9394-34c8-49a7-a72a-6c2cd1da83ca"
+}
+
+# Then run:
+# terraform plan -generate-config-out="generated.tf"
